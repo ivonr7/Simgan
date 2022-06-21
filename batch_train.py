b7ac655d453e227792ab54e46f278eaa0ee8ee65
William Choi <woongcheol.choi@gmail.com>
2022-06-20 17:38:52 -0700

    increasing batch size

--- batch_train.py
+++ batch_train.py
@@ -6,7 +6,7 @@ import click
 @click.command()
 @click.option('--batch-size',default=100,help='How many images are passed into the discriminator')
 def train_models(batch_size):
-    real_data=ld.Data(batch_size//2)
+    real_data=ld.Data(batch_size)
     real_data.get_img()
     real_data.show_img()
 
